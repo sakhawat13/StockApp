45d7f3c96c1548bffac6c59692a6e6a87e263657
sakhawat13 <sakhawat1305@gmail.com>
2022-05-11 00:20:09 +0600

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,128 @@
+import pickle
+
+
+# In[2]:
+
+
+import pandas as pd 
+import datetime
+import yfinance as yf
+import numpy as np
+#import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+#import matplotlib.pyplot as plt
+import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
+
+
+# In[3]:
+
+
+filename = 'classifier_model.sav'
+clf = pickle.load(open(filename, 'rb'))
+
+
+# In[4]:
+
+
+
+
+today = datetime.date.today()
+lastfive = today - datetime.timedelta(days=23)
+
+day = today.strftime ("%d/%m/%Y")
+five = lastfive.strftime ("%d/%m/%Y")
+
+
+# In[5]:
+
+
+import investpy
+
+
+# In[6]:
+
+
+stock_df = investpy.get_stocks_overview(country="Bangladesh", 
+                        as_json=False, 
+                        n_results=1000)
+
+data = investpy.get_stock_historical_data(stock='AAPL',
+                                        country='United States',
+                                        from_date='01/01/2010',
+                                        to_date='01/01/2020')
+
+
+st.write(data)
+# In[7]:
+
+#st = list[[]]
+
+option = list(( stock_df["name"]).unique())
+
+opt = st.multiselect(
+     'Which companies would you like?',
+     (option))
+
+st.write('You selected:', opt)
+
+for index, item in enumerate(opt):
+    opt[index] = stock_df.loc[stock_df["name"]==item]["symbol"].values[0]
+
+ 
+st.write(opt)
+
+
+
+submit = st.button("Submit")
+nt = ["ORIN", "PEIN" , "SALV", "FWCR", "HPPR", "ASLD","AFAM","BDCO",
+      "BDAL", "BDWE","KHCH","ANWG","ATBG","QASE","KHUL","BHLD","DESH"
+]
+if submit:
+  merged = pd.DataFrame()
+  for s in nt:
+      df4 = investpy.get_stock_historical_data(stock= s,
+                                        country='Bangladesh',
+                                        from_date="01/01/2007",
+                                        to_date= day)
+      df4["VolAvgNDays"] = df4["Volume"].rolling(15).mean()
+      df4 = df4[::-1]
+      df4["LP"] = df4["Close"].shift(-1)
+      df4["Change"] = ((df4["Close"]-df4["LP"])/df4["LP"])*100
+      df4 = df4[df4['VolAvgNDays'].notna()]
+      pred1 = clf.predict(df4[["Close","Volume","VolAvgNDays","Change"]])
+      df4["pred"] = pred1
+      df4["Name"] = s
+      df5 = df4.head(4)
+      merged = pd.concat([merged, df5], ignore_index=True)
+      
+  def aggrid_interactive_table(df: pd.DataFrame):
+      """Creates an st-aggrid interactive table based on a dataframe.
+
+      Args:
+          df (pd.DataFrame]): Source dataframe
+
+      Returns:
+          dict: The selected row
+      """
+      options = GridOptionsBuilder.from_dataframe(
+          df, enableRowGroup=True, enableValue=True, enablePivot=True
+      )
+
+      options.configure_side_bar()
+
+      options.configure_selection("single")
+      selection = AgGrid(
+          df,
+          enable_enterprise_modules=True,
+          gridOptions=options.build(),
+          theme="dark",
+          update_mode=GridUpdateMode.MODEL_CHANGED,
+          allow_unsafe_jscode=True,
+      )
+      return selection
+  selection = aggrid_interactive_table(df=merged)
+  if selection:
+      st.write("You selected:")
+      st.json(selection["selected_rows"])
