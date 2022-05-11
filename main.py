6d01346ccb18e92fa598e617308db42c030e2760
sakhawat13 <sakhawat1305@gmail.com>
2022-05-11 13:07:01 +0600

    Update main.py

--- main.py
+++ main.py
@@ -86,33 +86,31 @@ if submit:
       df4["Name"] = s
       df5 = df4.head(4)
       merged = pd.concat([merged, df5], ignore_index=True)
+      def aggrid_interactive_table(df: pd.DataFrame):
+        """Creates an st-aggrid interactive table based on a dataframe.
+        Args:
+            df (pd.DataFrame]): Source dataframe
+        Returns:
+            dict: The selected row
+        """
+        options = GridOptionsBuilder.from_dataframe(
+            df, enableRowGroup=True, enableValue=True, enablePivot=True
+        )
 
-def aggrid_interactive_table(df: pd.DataFrame):
-    """Creates an st-aggrid interactive table based on a dataframe.
+        options.configure_side_bar()
 
-    Args:
-        df (pd.DataFrame]): Source dataframe
+        #options.configure_selection("single")
+        selection = AgGrid(
+            df,
+            enable_enterprise_modules=True,
+            gridOptions=options.build(),
+            theme="dark",
+            update_mode=GridUpdateMode.MODEL_CHANGED,
+            allow_unsafe_jscode=True,
+        )
+        return selection
+  selection = aggrid_interactive_table(df=merged)
+  #if selection:
+     #st.write("You selected:")
+     #st.json(selection["selected_rows"])
 
-    Returns:
-        dict: The selected row
-    """
-    options = GridOptionsBuilder.from_dataframe(
-        df, enableRowGroup=True, enableValue=True, enablePivot=True
-    )
-
-    options.configure_side_bar()
-
-    options.configure_selection("single")
-    selection = AgGrid(
-        df,
-        enable_enterprise_modules=True,
-        gridOptions=options.build(),
-        theme="dark",
-        update_mode=GridUpdateMode.MODEL_CHANGED,
-        allow_unsafe_jscode=True,
-    )
-    return selection
-selection = aggrid_interactive_table(df=merged)
-if selection:
-    st.write("You selected:")
-    st.json(selection["selected_rows"])
