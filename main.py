933b16f8085ab8881d60896d79bc3c2f0676ac7f
sakhawat13 <sakhawat1305@gmail.com>
2022-05-12 13:11:59 +0600

    Update main.py

--- main.py
+++ main.py
@@ -134,12 +134,12 @@ if submit:
                 
                             
                 theme="dark",
-                update_mode=GridUpdateMode.MODEL_CHANGED,
+                #update_mode=GridUpdateMode.MODEL_CHANGED,
                 allow_unsafe_jscode=True,
             )
             return selection
-            selection = aggrid_interactive_table(df=merged)
-       #if selection:
-         #st.write("You selected:")
-         #st.json(selection["selected_rows"])
+        selection = aggrid_interactive_table(df=merged)
+   #if selection:
+     #st.write("You selected:")
+     #st.json(selection["selected_rows"])
 
